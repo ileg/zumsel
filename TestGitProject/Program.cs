c0eeeff11dae804fae166c71614264674751a59a
vep2 <vep2@bfh.ch>
2013-02-01 15:15:37 +0100

    Added new message.

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -10,7 +10,7 @@ namespace TestGitProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("Mono rox!");
         }
     }
 }
